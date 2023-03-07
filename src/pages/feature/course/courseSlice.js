f8c0737863208237f1519ea63b9a601179766792
unknown <matasnim11@gmail.com>
2023-03-07 21:10:06 +0600

    6th may be

--- src/pages/feature/course/courseSlice.js
+++ src/pages/feature/course/courseSlice.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 const courseSlice = createApi({
     reducerPath : "course",
     baseQuery : fetchBaseQuery({
-        baseUrl : 'http://localhost:5001/api/v1/app/user',
+        baseUrl : 'https://spc-cc-server.vercel.app/api/v1/app/user',
     }),
     tagTypes : ['post'],
     endpoints : (builder) => ({}),
