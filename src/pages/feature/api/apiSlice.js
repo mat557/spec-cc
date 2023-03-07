2a9bbe238e96ac1d4a9a796a11f1812f721d6fe3
unknown <matasnim11@gmail.com>
2023-03-07 22:21:55 +0600

    6th

--- src/pages/feature/api/apiSlice.js
+++ src/pages/feature/api/apiSlice.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const apiSlice = createApi({
     reducerPath : "api",
     baseQuery : fetchBaseQuery({
-        baseUrl : 'https://spc-cc-server.vercel.app/api/v1/app/user',
+        baseUrl : 'http://localhost:5001/api/v1/app/user',
     }),
     endpoints : (builder) =>({}),
 });
